06e34ce44150345f03b204410ca6392e734701b0
Nabanita Chatterjee <nchatterjee44@gmail.com>
2022-01-19 00:43:29 +0530

    Adding health controller

--- src/main/java/com/gameboard/tictactoe/controller/TicTacToeController.java
+++ src/main/java/com/gameboard/tictactoe/controller/TicTacToeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TicTacToeController {
@@ -21,18 +23,25 @@ public class TicTacToeController {
     }
 
     @PostMapping(path = "/board")
-    public TicTacToeIO createGameBoard(HttpServletRequest request,@RequestBody(required = false) GameType gameType) {
-        return ticTacToeService.createGameBoard(gameType != null ? gameType.getGameType() : "SINGLEPLAYER",request);
+    public TicTacToeIO createGameBoard(HttpServletRequest request, @RequestBody(required = false) GameType gameType) {
+        return ticTacToeService.createGameBoard(gameType != null ? gameType.getGameType() : "SINGLEPLAYER", request);
     }
 
     @GetMapping(path = "/board/sessionId/{sessionId}")
-    public TicTacToeIO getGameBoard(HttpSession httpSession,@PathVariable("sessionId") int sessionId) {
-        return ticTacToeService.getCurrentGameBoard(httpSession,sessionId);
+    public TicTacToeIO getGameBoard(HttpSession httpSession, @PathVariable("sessionId") int sessionId) {
+        return ticTacToeService.getCurrentGameBoard(httpSession, sessionId);
     }
 
     @PutMapping(path = "/board/player/{player}/sessionId/{sessionId}")
-    public TicTacToeIO putPlayerSymbol(@PathVariable(value = "player") int player,@PathVariable(value = "sessionId") int sessionId, HttpSession httpSession, @RequestBody PlayerMove playerMove) {
+    public TicTacToeIO putPlayerSymbol(@PathVariable(value = "player") int player, @PathVariable(value = "sessionId") int sessionId, HttpSession httpSession, @RequestBody PlayerMove playerMove) {
         return ticTacToeService.putPlayerSymbolAtPosition(player, sessionId, httpSession, playerMove);
+    }
+
+    @GetMapping("/")
+    public Map<String, String> health() {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "up");
+        return response;
     }
 
 }
